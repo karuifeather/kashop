c3d76a98f3f1df1c0ca1d7de4efc9e49b4cb386b
karuifeather <aryalaashaya@gmail.com>
2021-02-23 08:02:38 +0545

    added global error handler

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,15 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 
 import productRouter from './routes/productRoutes.js';
+import globalErrorHandler from './controllers/errorHandler.js';
 
 dotenv.config();
 const app = express();
 
 if (process.env.NODE_ENV === 'development') app.use(morgan('tiny'));
 
-
 app.use('/api/v1/products', productRouter);
+
+app.use(globalErrorHandler);
 
 export default app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,9 +1,11 @@
 import Product from '../models/productModel.js';
+import AppError from '../utils/appError.js';
+import asyncHandler from '../utils/asyncHandler.js';
 
 // @desc    Fetch all products
 // @route   /api/v1/products
 // @access  Public
-export const getProducts = async (req, res, next) => {
+export const getProducts = asyncHandler(async (req, res, next) => {
   const products = await Product.find({});
 
   res.status(200).json({
@@ -11,16 +13,20 @@ export const getProducts = async (req, res, next) => {
     results: products.length,
     data: { products },
   });
-};
+});
 
 // @desc    Fetch product by ID
 // @route   /api/v1/products/:id
 // @access  Public
-export const getProduct = async (req, res, next) => {
+export const getProduct = asyncHandler(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new AppError('No product with that ID can be found.', 404));
+  }
 
   res.status(200).json({
     status: 'success',
     data: { product },
   });
-};
+});
