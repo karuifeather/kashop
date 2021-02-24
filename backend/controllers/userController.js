83e6481359e45ab3d0f5ddd532bef44897c19047
karuifeather <aryalaashaya@gmail.com>
2021-02-24 13:03:30 +0545

    removed an import

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,6 @@
 import User from '../models/userModel.js';
 import AppError from '../utils/appError.js';
 import asyncHandler from '../utils/asyncHandler.js';
-import generateToken from '../utils/generateToken.js';
 
 // @desc    Get logged in user profile
 // @route   GET /api/v1/users/profile
